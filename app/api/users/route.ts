df634ada6a583fa67221d79b28925ce43beee065
hod284 <61046278+hod284@users.noreply.github.com>
2026-01-09 12:45:07 +0900

    1212
    1212121

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -31,52 +31,7 @@ export async function GET(request : Request) {
         );
     }
 }
-// 새사용자 생성
-export async function POST(request: Request)
-{
-    try
-    {
-        const body : DbUser = await request.json() as DbUser;
-        const {name , email,role,status} = body;
-        // 유효성 검사
-        if(!name || !email)
-        {
-            return NextResponse.json(
-                {success : false , error : "이메일이나 이름이 없습니다" },
-                {status :400}
-           );
-        }
-    
-        // returning은 결과 값을 돌려받는 부분
-        const result = await query(`
-            INSERT  INTO users (name,email,role,status,joined)
-            VALUES ($1,$2,$3,$4,NOW())
-            RETURNING * `,[name,email,role||'사용자',status||'활성']);
-        const response : ApiResponse<DbUser> ={
-            success :true,
-            data : result.rows[0]
-        }
-        return NextResponse.json(response,{status :201});  
-    }
-    catch(error : unknown)
-     {
-         console.error("사용자 생성 에러 : ",error);
-         if(isPostgresError(error))
-         {
-            if(error.code === '23505')
-            {
-                return NextResponse.json(
-                    {success : false , error : "이미 존재하는 이메일입니다"},
-                    {status : 409}
-                );
-            }
-         }
-         return NextResponse.json(
-            {success : false , error : '사용자 생성 실패'},
-            {status :500}
-         );
-     }
-}
+
 // 사용자 수정
 export async function PUT(request: Request)
 {
